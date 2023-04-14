7ca5de9074f5a8e00a6558b438068dc4b4e3d831
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-14 17:52:24 +0200

    added a quiz array for export

---
+++ src/Questions/questions.js
@@ -0,0 +1,28 @@
+const quiz = [  {    
+    question: "What is the result of 2 + 2?",    
+    choices: ["3", "4", "5", "6"],
+    answer: "4"
+  },
+  {
+    question: "What is the output of console.log('Hello, world!')?",
+    choices: ["Hello, world!", "undefined", "null", "NaN"],
+    answer: "Hello, world!"
+  },
+  {
+    question: "Which of the following is NOT a JavaScript data type?",
+    choices: ["string", "number", "boolean", "array"],
+    answer: "array"
+  },
+  {
+    question: "What is the result of '5' + 2?",
+    choices: ["7", "52", "undefined", "NaN"],
+    answer: "52"
+  },
+  {
+    question: "What does the 'typeof' operator return when applied to null?",
+    choices: ["null", "undefined", "object", "boolean"],
+    answer: "object"
+  }
+];
+
+export default quiz
