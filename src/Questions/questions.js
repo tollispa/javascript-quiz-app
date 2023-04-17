000ed6c3cb581d7f7cbca00de59fd88e05591619
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-17 12:10:27 +0200

    Added more categories to my questions

--- src/Questions/questions.js
+++ src/Questions/questions.js
@@ -1,4 +1,4 @@
-const quiz = [  {    
+export const javaScriptQuiz = [  {    
     question: "What is the result of 2 + 2?",    
     choices: ["3", "4", "5", "6"],
     answer: "4"
@@ -25,4 +25,57 @@ const quiz = [  {
   }
 ];
 
-export default quiz
+export const disneyQuiz = [  {    question: "What is the name of the mermaid in Disney's The Little Mermaid?",   
+   choices: ["Ariel", "Jasmine", "Belle", "Cinderella"],
+    answer: "Ariel"
+  },
+  {
+    question: "In which Disney movie does the character Mulan appear?",
+    choices: ["Pocahontas", "Mulan", "Moana", "Brave"],
+    answer: "Mulan"
+  },
+  {
+    question: "What is the name of the main character in Disney's Beauty and the Beast?",
+    choices: ["Belle", "Aurora", "Jasmine", "Cinderella"],
+    answer: "Belle"
+  },
+  {
+    question: "What is the name of the sister duo who are known for their red hair in Disney's Frozen?",
+    choices: ["Anna and Elsa", "Aurora and Jasmine", "Merida and Moana", "Rapunzel and Tiana"],
+    answer: "Anna and Elsa"
+  },
+  {
+    question: "What is the name of the villain in Disney's Sleeping Beauty?",
+    choices: ["Maleficent", "Ursula", "Jafar", "Gaston"],
+    answer: "Maleficent"
+  }
+];
+
+export const gotQuiz = [  {    question: "Who is the main character in Game of Thrones?",    choices: ["Jon Snow", "Tyrion", "Daenerys", "Cersei"],
+answer: "Jon Snow"
+},
+{
+question: "What is the name of the fictional continent in Game of Thrones?",
+choices: ["Essos", "Westeros", "Sothoryos", "Ulthos"],
+answer: "Westeros"
+},
+{
+question: "What is the name of the leader of the Night's Watch in Game of Thrones?",
+choices: ["Jon Snow", "Samwell Tarly", "Jeor Mormont", "Eddison Tollett"],
+answer: "Jeor Mormont"
+},
+{
+question: "Who is the youngest Stark child in Game of Thrones?",
+choices: ["Sansa", "Arya", "Bran", "Rickon"],
+answer: "Rickon"
+},
+{
+question: "What is the name of the sword that Jon Snow wields in Game of Thrones?",
+choices: ["Needle", "Longclaw", "Ice", "Oathkeeper"],
+answer: "Longclaw"
+}
+];
+
+
+
+
