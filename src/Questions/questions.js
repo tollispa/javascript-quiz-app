89a1db24c41b6f3461e6cf9012ba0ff9a4241962
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-18 13:52:04 +0200

    added some more questions

--- src/Questions/questions.js
+++ src/Questions/questions.js
@@ -27,10 +27,83 @@ export const javaScriptQuiz = [    {
     question: "What does the 'typeof' operator return when applied to null?",
     choices: ["null", "undefined", "object", "boolean"],
     answer: "object"
+  }, {
+    question: "What is the difference between 'let' and 'var' in JavaScript?",
+    choices: [
+      "'let' has block scope, while 'var' has function scope",
+      "'var' has block scope, while 'let' has function scope",
+      "'let' and 'var' are interchangeable and can be used interchangeably",
+      "there is no difference between 'let' and 'var'"
+    ],
+    answer: "'let' has block scope, while 'var' has function scope"
+  },
+  {
+    question: "What is the difference between '==' and '===' in JavaScript?",
+    choices: [
+      "'==' performs type coercion before comparing two values, while '===' does not",
+      "'===' performs type coercion before comparing two values, while '==' does not",
+      "'==' and '===' are interchangeable and can be used interchangeably",
+      "there is no difference between '==' and '==='"
+    ],
+    answer: "'==' performs type coercion before comparing two values, while '===' does not"
+  },
+  {
+    question: "What is a closure in JavaScript?",
+    choices: [
+      "A closure is a function that has access to variables in its outer (enclosing) function's scope chain",
+      "A closure is a function that has access to global variables",
+      "A closure is a function that has access to local variables declared within its own function body",
+      "A closure is a function that returns another function"
+    ],
+    answer: "A closure is a function that has access to variables in its outer (enclosing) function's scope chain"
+  },
+  {
+    question: "What is the 'this' keyword in JavaScript?",
+    choices: [
+      "'this' refers to the current object that a method or constructor is being called on",
+      "'this' refers to the global object",
+      "'this' refers to the parent object of the current object",
+      "'this' is a reserved keyword in JavaScript with no specific meaning"
+    ],
+    answer: "'this' refers to the current object that a method or constructor is being called on"
   }
 ];
 
-export const disneyQuiz = [  {    question: "What is the name of the sword in 'The Sword in the Stone'?",    choices: [      "Excalibur",      "Clarent",      "Caliburn",      "Pridwen"    ],
+export const disneyQuiz = [ 
+  {
+    question: "In 'The Little Mermaid', what does Ariel trade to Ursula in order to become human?",
+    choices: ["Her voice", "Her soul", "Her family", "Her beauty"],
+    answer: "Her voice"
+  },
+  {
+    question: "In the Disney movie 'The Fox and the Hound', what is the name of the old female owl who helps Big Mama look after Tod?",
+    choices: ["Widow Tweed", "Mrs. Hooter", "Mama Owl", "Aunt Prissy"],
+    answer: "Widow Tweed"
+  },
+  {
+    question: "In 'The Lion King', what is the name of Scar's loyal hyena sidekick?",
+    choices: ["Shenzi", "Banzai", "Ed", "Kamari"],
+    answer: "Shenzi"
+  },
+  {
+    question: "What is the name of the prince in Beauty and the Beast?",
+    choices: ["Prince Adam", "Prince Charming", "Prince Eric", "Prince Phillip"],
+    answer: "Prince Adam"
+  },
+  {
+    question: "What is the name of the cat in Cinderella?",
+    choices: ["Lucifer", "Salem", "Tom", "Garfield"],
+    answer: "Lucifer"
+  },
+  {
+    question: "What is the name of the horse in Tangled?",
+    choices: ["Maximus", "Phillip", "Samson", "Khan"],
+    answer: "Maximus"
+  },
+  {    question: "In what year was Snow White and the Seven Dwarfs released?",    choices: ["1937", "1940", "1955", "1960"],
+  answer: "1937"
+},
+  {    question: "What is the name of the sword in 'The Sword in the Stone'?",    choices: [      "Excalibur",      "Clarent",      "Caliburn",      "Pridwen"    ],
     answer: "Excalibur"
   },
   {
@@ -133,7 +206,61 @@ export const javaQuestions = [  {    question: "What keyword is used to define a
     question: "Which keyword is used to define a class in Java?",
     choices: ["class", "interface", "extends", "implements"],
     answer: "class"
+  },
+  {
+    question: "What is a static method in Java?",
+    choices: [
+      "A method that can only be called from within the same class.",
+      "A method that can be called without creating an instance of the class.",
+      "A method that is synchronized with other methods.",
+      "A method that is declared with the `static` keyword."
+    ],
+    answer: "A method that can be called without creating an instance of the class."
+  },
+  {
+    question: "What is the difference between a `Stack` and a `Queue` in Java?",
+    choices: [
+      "A `Stack` is LIFO (Last-In-First-Out), while a `Queue` is FIFO (First-In-First-Out).",
+      "A `Stack` is FIFO (First-In-First-Out), while a `Queue` is LIFO (Last-In-First-Out).",
+      "A `Stack` and a `Queue` are the same data structure.",
+      "A `Stack` and a `Queue` can be used interchangeably."
+    ],
+    answer: "A `Stack` is LIFO (Last-In-First-Out), while a `Queue` is FIFO (First-In-First-Out)."
+  },
+  {
+    question: "What is the difference between an abstract class and an interface in Java?",
+    choices: [
+      "An abstract class can have method implementations, while an interface cannot.",
+      "An interface can have method implementations, while an abstract class cannot.",
+      "An abstract class can be instantiated, while an interface cannot.",
+      "An interface can be extended, while an abstract class cannot."
+    ],
+    answer: "An abstract class can have method implementations, while an interface cannot."
+  },
+  {
+    question: "What is the difference between `StringBuilder` and `StringBuffer` in Java?",
+    choices: [
+      "`StringBuilder` is not thread-safe, while `StringBuffer` is thread-safe.",
+      "`StringBuffer` is not thread-safe, while `StringBuilder` is thread-safe.",
+      "`StringBuilder` and `StringBuffer` are the same class.",
+      "`StringBuilder` and `StringBuffer` are used for different data types."
+    ],
+    answer: "`StringBuilder` is not thread-safe, while `StringBuffer` is thread-safe."
+  },
+  {
+    question: "What is the difference between a `private` and `protected` method in Java?",
+    choices: [
+      "A `private` method can only be accessed within the same class, while a `protected` method can also be accessed within subclasses.",
+      "A `protected` method can only be accessed within the same class, while a `private` method can also be accessed within subclasses.",
+      "A `private` method can be accessed from anywhere, while a `protected` method can only be accessed within the same package.",
+      "A `protected` method can be accessed from anywhere, while a `private` method can only be accessed within the same package."
+    ],
+    answer: "A `private` method can only be accessed within the same class, while a `protected` method can also be accessed within subclasses."
   }
+  
+  
+  
+  
 ];
 
 export const footballQuestions = [  {    question: "Which player won the European Golden Shoe in the 2019-20 season?",    choices: ["Lionel Messi", "Cristiano Ronaldo", "Robert Lewandowski", "Ciro Immobile"],
@@ -233,6 +360,30 @@ export const friendsQuiz = [  {    question: "What was the name of the original 
     question: "What is the name of the brand of men's lipstick that Joey tries to promote?",
     choices: ["Lip Service", "Manscara", "Joey's Lipstick", "Lipstick for Men"],
     answer: "Manscara"
+  },{
+    question: "What is Chandler's middle name?",
+    choices: ["Muriel", "Marcel", "Matthew", "Monica"],
+    answer: "Muriel"
+  },
+  {
+    question: "What instrument does Phoebe play?",
+    choices: ["Guitar", "Piano", "Drums", "Bass"],
+    answer: "Guitar"
+  },
+  {
+    question: "What is the name of the dog that Phoebe temporarily keeps after its owner dies?",
+    choices: ["Clunkers", "Snickers", "Fluffernutter", "Bob"],
+    answer: "Clunkers"
+  },
+  {
+    question: "What is the name of the character that Ross accidentally kisses in the coffee shop?",
+    choices: ["Cheryl", "Carol", "Susan", "Emily"],
+    answer: "Cheryl"
+  },
+  {
+    question: "What is the name of Joey's imaginary friend?",
+    choices: ["Maurice", "Mortimer", "Martin", "Max"],
+    answer: "Maurice"
   }
 ];
 
